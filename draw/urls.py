c0e56eff34fdf6ef440b8776e76866af298a0fd0
williamcheng1998 <williamcheng1998@gmail.com>
2019-07-28 03:30:30 -0400

    Done

--- draw/urls.py
+++ draw/urls.py
@@ -3,8 +3,13 @@ from django.conf.urls import url
 
 from . import views
 
+from django.urls import path
+
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^(?P<room_name>[^/]+)/$', views.room, name='room'),
+    # url(r'^(?P<room_name>[^/]+)/$', views.room, name='room'),
+    path('index2/', views.index2, name='index2'),
+     path('index3/', views.index3, name='index3'),
+  
 ]
 
--- draw/views.py
+++ draw/views.py
@@ -4,8 +4,13 @@ import json
 
 def index(request):
     return render(request, 'draw/index.html', {})
+def index2(request):
+    return render(request, 'draw/index2.html')
+  
+def index3(request):
+    return render(request, 'draw/index3.html')
 
-def room(request, room_name):
-    return render(request, 'draw/room.html', {
-        'room_name_json': mark_safe(json.dumps(room_name))
-    })
+# def room(request, room_name):
+    # return render(request, 'draw/room.html', {
+       # 'room_name_json': mark_safe(json.dumps(room_name))
+    # }) 
